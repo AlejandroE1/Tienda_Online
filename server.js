b64468064639a864cbad751dfad4df204a3a2b5c
LizethLeyva <lizeth.leyva15@gmail.com>
2024-11-06 20:55:35 -0500

    Add qualification and productos

--- server.js
+++ server.js
@@ -35,21 +35,6 @@ db.query(
   }
 );
 
-// Crear la tabla 'productos' si no existe----------------
-db.query(
-  `CREATE TABLE IF NOT EXISTS productos (
-  id int(11) AUTO_INCREMENT PRIMARY KEY,
-  nombre_producto varchar(255) NOT NULL,
-  descrip_producto varchar(255) NOT NULL,
-  valor int(100) NOT NULL,
-  marca varchar(255) NOT NULL
-);`,
-  (err) => {
-    if (err) throw err;
-    console.log("Tabla 'productos' creada o verificada");
-  }
-);
-
 // Crear una instancia de Express
 const app = express();
 const PORT = 3000;
@@ -69,24 +54,6 @@ app.post("/usuarios", (req, res) => {
         return;
       }
       res.status(201).json({id: result.insertId,nombres,apellidos,email,telefono,nickname,});
-    }
-  );
-});
-
-// Ruta para crear un nuevo registro de PRODUCTO-----------
-app.post("/productos", (req, res) => {
-  const { nombre_producto, descrip_producto, valor, marca} = req.body;
-  const sql =
-    "INSERT INTO productos (nombre_producto, descrip_producto, valor, marca) VALUES (?, ?, ?, ?)";
-  db.query(
-    sql,
-    [nombre_producto, descrip_producto, valor, marca],
-    (err, result) => {
-      if (err) {
-        res.status(500).send("Error al crear producto");
-        return;
-      }
-      res.status(201).json({id: result.insertId, nombre_producto, descrip_producto, valor, marca,});
     }
   );
 });
@@ -118,34 +85,6 @@ app.get("/usuarios/:id", (req, res) => {
   });
 });
 
-// Ruta para obtener todas los PRODUCTO-------------------------
-app.get("/productos", (req, res) => {
-  db.query("SELECT * FROM productos", (err, results) => {
-    if (err) {
-      res.status(500).send("Error al obtener producto");
-      return;
-    }
-    res.json(results);
-  });
-});
-
-// Ruta para obtener PRODUCTO por ID-----------------------------
-app.get("/productos/:id", (req, res) => {
-  const sql = "SELECT * FROM productos WHERE id = ?";
-  db.query(sql, [req.params.id], (err, results) => {
-    if (err) {
-      res.status(500).send("Error al obtener producto");
-      return;
-    }
-    if (results.length === 0) {
-      res.status(404).send("producto no encontrado");
-      return;
-    }
-    res.json(results[0]);
-  });
-});
-
-
 // Ruta para modificar un usuario
 app.put("/usuarios/:id", (req, res) => {
   const { nombres, apellidos, email, telefono, nickname } = req.body;
@@ -168,28 +107,6 @@ app.put("/usuarios/:id", (req, res) => {
   );
 });
 
-// Ruta para modificar un PRODUCTO----------------------
-app.put("/productos/:id", (req, res) => {
-  const { nombre_producto, descrip_producto, valor, marca } = req.body;
-  const sql =
-    "UPDATE productos SET nombre_producto = ?, descrip_producto = ?, valor = ?, marca = ? WHERE id = ?";
-  db.query(
-    sql,
-    [nombre_producto, descrip_producto, valor, marca, req.params.id],
-    (err, result) => {
-      if (err) {
-        res.status(500).send("Error al actualizar producto");
-        return;
-      }
-      if (result.affectedRows === 0) {
-        res.status(404).send("producto no encontrado");
-        return;
-      }
-      res.json({id: req.params.id,nombre_producto, descrip_producto, valor, marca,});
-    }
-  );
-});
-
 // Ruta para eliminar un USUARIO
 app.delete("/usuarios/:id", (req, res) => {
   const sql = "DELETE FROM usuarios WHERE id = ?";
@@ -203,23 +120,6 @@ app.delete("/usuarios/:id", (req, res) => {
       return;
     }
     res.send("usuario eliminado");
-  });
-});
-
-
-// Ruta para eliminar un PRODUCTO--------------------------
-app.delete("/productos/:id", (req, res) => {
-  const sql = "DELETE FROM productos WHERE id = ?";
-  db.query(sql, [req.params.id], (err, result) => {
-    if (err) {
-      res.status(500).send("Error al eliminar producto");
-      return;
-    }
-    if (result.affectedRows === 0) {
-      res.status(404).send("Producto no encontrado");
-      return;
-    }
-    res.send("Producto eliminado");
   });
 });
 
